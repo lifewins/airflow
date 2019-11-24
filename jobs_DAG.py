78f5947cec185e830095800385e5af92e6e7392b
Andrey Razdolsky <Andrey.Razdolsky@kohls.com>
2019-11-24 07:09:50 -0800

    26 step

--- jobs_DAG.py
+++ jobs_DAG.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.trigger_rule import TriggerRule
+
+ex_file = Variable.get('ex_file', default_var='/Users/arazdolskiy/Development/airflow/tmp/ex.txt')
 
 default_args = {
     'owner': 'AndrewR',
@@ -15,38 +18,48 @@ default_args = {
     'email_on_failure': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
-    'catchup': False
+    'catchup': False,
+    'priority_weight': 150
 }
 
 config = {
     'andrewR_m_1': {
-        'schedule_interval': "@hourly",
+        'schedule_interval': "@once",
         "start_date": datetime(2019, 11, 7),
         "db_table": "table_task_1"
     },
     'andrewR_h_2': {
-        'schedule_interval': "@hourly",
+        'schedule_interval': "@once",
         "start_date": datetime(2019, 11, 7),
         "db_table": "table_task_2"
 
     },
     'andrewR_d_3': {
-        'schedule_interval': "@daily",
+        'schedule_interval': "@once",
         "start_date": datetime(2019, 11, 7),
         "db_table": "table_task_3"
     }
 }
 
 
-def printDagTask1Str(*args, **kargs):
-    print("{} start processing tables in database: {}".format(kargs["dag_id"], kargs["database"]))
+def printDagTask1Str(**kwargs):
+    print("{} start processing tables in database: {}".format(kwargs["dag_id"], kwargs["database"]))
 
 
 def check_table_exists():
-    if True:
-        return "skip_table_creation"
-    else:
+    if Variable.get("should_create_table", default_var='True') == 'True':
         return "create_table"
+    else:
+        return "skip_table_creation"
+
+
+def pushXCom(*kargs, **kwargs):
+    task_instance = kwargs['task_instance']
+    task_instance.xcom_push(key="results", value=kwargs['run_id'])
+    task_instance.xcom_push(key="execution_ext", value=kwargs['ts'])
+    file = open(kwargs['ex_file'],'w')
+    file.write(kwargs['ts'])
+    file.close()
 
 
 def createDag(dagId, config, default_args):
@@ -55,22 +68,24 @@ def createDag(dagId, config, default_args):
               default_args=default_args,
               start_date=config[dagId]["start_date"],
               schedule_interval=config[dagId]["schedule_interval"])
+
     with dag:
         say_hello = PythonOperator(
             task_id="print_log_{}".format(dagId),
             python_callable=printDagTask1Str,
             op_kwargs={"dag_id": dag_ID, "database": default_args["database"]},
         )
-        inset_new_row = DummyOperator(
-            task_id="inset_new_row_{}".format(dag_ID),
-            trigger_rule=TriggerRule.ALL_DONE
-        )
-        query_the_table = DummyOperator(
-            task_id="query_the_table_{}".format(dag_ID)
+        print_user = BashOperator(
+            task_id="print_user",
+            bash_command='echo USER'
         )
         checkTable = BranchPythonOperator(
             task_id="checkTable",
             python_callable=check_table_exists,
+        )
+        inset_new_row = DummyOperator(
+            task_id="inset_new_row_{}".format(dag_ID),
+            trigger_rule=TriggerRule.ALL_DONE
         )
         skip_table_creation = BashOperator(
             task_id="skip_table_creation",
@@ -81,12 +96,14 @@ def createDag(dagId, config, default_args):
             bash_command=" echo 'create table' "
         )
 
-        print_user = BashOperator(
-            task_id="print_user",
-            bash_command='echo "$USER" '
+        query_the_table = PythonOperator(
+            task_id="query_the_table",
+            python_callable=pushXCom,
+            provide_context=True,
+            op_kwargs={'run_id' : "{{ run_id }} ended", 'ts': "{{ ts }}", 'ex_file' : ex_file}
         )
 
-        say_hello >> print_user >> checkTable >> [skip_table_creation, create_table] >> inset_new_row >> query_the_table
+    say_hello >> print_user >> checkTable >> [skip_table_creation, create_table] >> inset_new_row >> query_the_table
 
     return dag
 
--- trigger_dag.py
+++ trigger_dag.py
@@ -5,43 +5,121 @@ from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.models import Variable
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
 
-dags = ["dag_andrewR_m_1_table_task_1", "dag_andrewR_h_2_table_task_2", "dag_andrewR_d_3_table_task_3"]
+dagToCall = 'dag_andrewR_m_1_table_task_1'
 
-dagToCall = 'weekdayJobs'
+main_dag_id = "calling_dag"
+default_path = '/Users/arazdolskiy/Development/airflow/tmp/'
 
 default_args = {
     'dagToCall': dagToCall,
-    'run_file': '/Users/arazdolskiy/Development/airflow/tmp/run',
+    'run_file': default_path + 'run',
     'owner': 'AndrewR',
-    'start_date': datetime(2019, 11, 15),
+    'start_date': datetime(2019, 11, 17),
     'catchup': False,
     'concurrency': 1,
-    'depends_on_past': True
+    'depends_on_past': False,
+    'priority_weight': 100
 }
 
 path = Variable.get('run_file', default_var=default_args['run_file'])
-dag = DAG(
-    dag_id="calling_dag",
-    schedule_interval="@daily",
+ex_file = Variable.get('ex_file', default_var='/Users/arazdolskiy/Development/airflow/tmp/ex.txt')
+
+
+def print_ext_result(*kargs, **kwargs):
+    task_instance = kwargs['task_instance']
+    results = task_instance.xcom_pull(dag_id=dagToCall, task_ids="query_the_table", key='results')
+    print("'{}' dag result={}".format(dagToCall, results))
+    print(kwargs)
+
+
+def exec_delta(this_dag_scheduled_date):
+    print("Master DAG scheduled date={}".format(this_dag_scheduled_date))
+    date_str = ""
+    try:
+        file = open(ex_file, 'r')
+        date_str = file.readline(1)
+        ex_date = datetime.fromisoformat(date_str)
+        file.close()
+        print("Slave DAG scheduled date={}".format(ex_date))
+        return ex_date
+    except:
+        print("Wrong date={} in file={}".format(date_str, ex_file))
+
+
+def build_process_result_sub_dag(main_dag, default_args):
+    s_dag = DAG(
+        dag_id="{}.{}".format(main_dag, 'process_result_sub_dag'),
+        default_args=default_args,
+        schedule_interval='@daily'
+    )
+
+    external_dag_sensor = ExternalTaskSensor(
+        task_id='external_dag_sensor',
+        external_dag_id=dagToCall,
+        external_task_id='None',
+        execution_date_fn=exec_delta,
+        dag=s_dag
+    )
+    ex_file_sensor = FileSensor(
+        task_id="ex_file_sensor",
+        filepath=ex_file,
+        dag=s_dag
+    )
+    print_external_dag_result = PythonOperator(
+        task_id="print_external_dag_result",
+        python_callable=print_ext_result,
+        provide_context=True,
+        dag=s_dag
+    )
+    remove_trigger_file = BashOperator(
+        task_id="remove_trigger_file",
+        bash_command="rm -f {}".format(path),
+        dag=s_dag
+    )
+    create_finished_file = BashOperator(
+        task_id="create_finished_file",
+        bash_command="touch " + default_path + "finished_#{{ ts_nodash }}",
+        dag=s_dag
+    )
+
+    ex_file_sensor >> external_dag_sensor >> print_external_dag_result >> remove_trigger_file >> create_finished_file
+    return s_dag
+
+
+# main dag
+main_dag = DAG(
+    dag_id=main_dag_id,
     default_args=default_args,
+    schedule_interval='@daily'
 )
 
-with dag:
-    check_run_file = FileSensor(
-        task_id="check_run_file",
-        poke_interval=1,
-        default_args=default_args,
-        filepath=path
-    )
-    trigger_task_1 = TriggerDagRunOperator(
-        task_id="trigger_task_0",
-        trigger_dag_id=default_args['dagToCall']
-    )
+remove_ex_file = BashOperator(
+    task_id="remove_ex_file",
+    bash_command="rm -f {}".format(ex_file),
+    dag=main_dag
+)
 
-    remove_run_file = BashOperator(
-        task_id="remove_run_file",
-        bash_command="rm -f {}".format(path)
-    )
+check_run_file = FileSensor(
+    task_id="check_run_file",
+    poke_interval=1,
+    filepath=path,
+    dag=main_dag
+)
 
-    check_run_file >> trigger_task_1 >> remove_run_file
+triggering_external_dag = TriggerDagRunOperator(
+    task_id="triggering_external_dag",
+    trigger_dag_id=dagToCall,
+    dag=main_dag
+)
+
+process_result_sub_dag = SubDagOperator(
+    task_id='process_result_sub_dag',
+    subdag=build_process_result_sub_dag(main_dag_id, default_args),
+    dag=main_dag
+)
+
+remove_ex_file >> check_run_file >> triggering_external_dag >> process_result_sub_dag
